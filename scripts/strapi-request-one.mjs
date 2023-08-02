bf593b14be3d6af3dddd1692c203857f508561a9
Daddyjohn63 <john@webandprosper.co.uk>
2023-08-02 13:40:33 +0100

    upto fetch refactor

---
+++ scripts/strapi-request-one.mjs
@@ -0,0 +1,24 @@
+import { writeFileSync } from 'node:fs';
+import qs from 'qs';
+
+const url =
+  'http://localhost:1337/api/reviews' +
+  '?' +
+  qs.stringify(
+    {
+      fields: ['slug', 'title', 'subtitle', 'publishedAt'],
+      filters: { slug: { $eq: 'hades-2018' } },
+      fields: ['slug', 'title', 'subtitle', 'publishedAt', 'body'],
+      populate: { image: { fields: ['url'] } },
+      // sort: ['publishedAt:desc'],
+      // pagination: { pageSize: 6 },
+      pagination: { pageSize: 1, withCount: false }
+    },
+    { encodeValuesOnly: true }
+  );
+console.log('url:', url);
+const response = await fetch(url);
+const body = await response.json();
+const formatted = JSON.stringify(body, null, 2);
+const file = 'scripts/strapi-response-one.json';
+writeFileSync(file, formatted, 'utf8');
